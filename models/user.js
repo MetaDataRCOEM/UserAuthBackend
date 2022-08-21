1936764df4743ffe8636fcb112ae20b525e3835f
maneshantanu20 <manesg@rknec.edu>
2022-08-21 17:06:07 +0530

    schema update : heroku_branch

--- models/user.js
+++ models/user.js
@@ -6,6 +6,26 @@ var userSchema = new Schema({
 		type: String,
 		require: true,
 	},
+	name: {
+		type: String,
+		require: true,
+	},
+	panchayat: {
+		type: String,
+		require: true,
+	},
+	block: {
+		type: String,
+		require: true,
+	},
+	district: {
+		type: String,
+		require: true,
+	},
+	state: {
+		type: String,
+		require: true,
+	},
 	mseID: {
 		type: String,
 		require: true,
